1ca64b7acae2e861dafa6d6e2f192dbdb3cc983e
Bobby Laptop <bobby@sankhagowit.com>
2017-05-09 00:58:36 -0500

    feat: add /restaraunts GET method displaying names and dummy links

--- vagrant/lesson1/webserver.py
+++ vagrant/lesson1/webserver.py
@@ -1,6 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -18,6 +25,26 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 print output  # for debugging
                 return
+
+            if self.path.endswith("/restaurants"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                restaurants = session.query(Restaurant).\
+                order_by(Restaurant.name)
+
+                output = ""
+                output += "<html><body>"
+                if restaurants:
+                    for restaurant in restaurants:
+                        output += "<p>%s<br><a href='#'>Edit</a><br><a href='#'>Delete</a></p>" % restaurant.name
+                output += "</body></html>"
+
+                self.wfile.write(output)
+                print output
+                return
+
 
             # search for url requests which end in /hola
             if self.path.endswith("/hola"):
