3bfd6308f29c7c449704922f48c6e6c76ad46193
Bobby Laptop <bobby@sankhagowit.com>
2017-05-09 01:53:02 -0500

    feat: Add view and controller to add a new restaraunt to the model

--- vagrant/lesson1/webserver.py
+++ vagrant/lesson1/webserver.py
@@ -36,13 +36,31 @@ class webServerHandler(BaseHTTPRequestHandler):
 
                 output = ""
                 output += "<html><body>"
+
                 if restaurants:
                     for restaurant in restaurants:
                         output += "<p>%s<br><a href='#'>Edit</a><br><a href='#'>Delete</a></p>" % restaurant.name
+
+                output += "<a href='/new'>Make a New Restaurant Here</a>"
                 output += "</body></html>"
 
                 self.wfile.write(output)
                 print output
+                return
+
+
+            if self.path.endswith("/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaraunt</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/new'><input name='restarauntName' type='text'><input type='submit' value='Create'></form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+
                 return
 
 
@@ -65,23 +83,49 @@ class webServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.end_headers()
+            if self.path.endswith("/new"):
+                self.send_response(301)
+                self.end_headers()
 
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    name = fields.get('restarauntName')
 
-            output = ""
-            output += "<html><body>"
-            output += "<h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
+                new_restaraunt = Restaurant(name=name[0])
+                session.add(new_restaraunt)
+                session.commit()
 
-            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+                restaurants = session.query(Restaurant).\
+                order_by(Restaurant.name)
+                output = ""
+                output += "<html><body><h2>Added %s to List</h2>" % name[0]
+
+                if restaurants:
+                    for restaurant in restaurants:
+                        output += "<p>%s<br><a href='#'>Edit</a><br><a href='#'>Delete</a></p>" % restaurant.name
+
+                output += "<a href='/new'>Make a New Restaurant Here</a>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+
+            if self.path.endswith("/edit"):
+                self.send_response(301)
+                self.end_headers()
+
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    name = fields.get('restarauntName')
+
+                output = ""
+                output += "<html><body>"
+                output += "<h2> OLD, how about this: </h2>"
+                output += "<h1> %s </h1>" % name[0]
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
 
         except:
             pass
