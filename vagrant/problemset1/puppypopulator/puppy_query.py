7cb54a6c2a4c55e402544a48e84f60255ceed82e
Bobby <bobby@sankhagowit.com>
2017-05-08 23:09:26 -0500

    feat: Add queries for problem set 1

--- vagrant/problemset1/puppypopulator/puppy_query.py
+++ vagrant/problemset1/puppypopulator/puppy_query.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from database_setup import Base, Shelter, Puppy
 
-engine = create_engine('sqlite///puppyshelter.db')
+engine = create_engine('sqlite///puppyshelter.db', echo = True)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
-sess = DBSession()
+session = DBSession()
