19de51d840d92508310f9c60381e23a008dc03c3
Bobby Laptop <bobby@sankhagowit.com>
2017-05-09 00:51:28 -0500

    test: Add file to interogate database to validate HTML

---
+++ vagrant/lesson1/db_queries.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+for restaurant in session.query(Restaurant).order_by(Restaurant.name):
+    print restaurant.name, restaurant.id
